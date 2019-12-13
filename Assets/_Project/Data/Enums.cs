703133ad2ef3a3a57a4fa0e3bc34e79b105cd6b8
RJ Perry <raymond.perry@maine.edu>
2019-12-13 17:03:43 -0500

    Had to Rollback some things... Committing Data structure I currently have so i can rollback the rest

---
+++ Assets/_Project/Data/Enums.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum ItemType{
+	/*
+	//If we use item subtypes
+	Weapon,
+	Armor,
+	Accessory
+	*/
+
+	//Weapons
+	Sword,
+	Bow,
+	Staff,
+	Mace,
+	TwoHandedSword,
+	//Armor
+	Heavy,
+	Light,
+	Robe,
+	Shield,
+	//Accessories
+	Ring,
+	Necklace,
+	Boots
+
+}
+//If we use item subtypes:
+/*
+public enum WeaponType{
+	Sword,
+	Bow,
+	Staff,
+	Mace,
+	TwoHandedSword,
+
+}
+
+public enum ArmorType{
+	Heavy,
+	Light,
+	Robe,
+	Shield
+}
+
+public enum AccessoryType{
+	Ring,
+	Necklace,
+	Boots
+}
+*/
+
+public enum ItemMaterial{
+	Wood,
+	Iron,
+	Bronze,
+	Silver,
+	Gold,
+	Leather,
+	Bone,
+	Cloth,
+}
---
+++ Assets/_Project/Data/Item.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Item")]
+public class Item: ScriptableObject   {
+    
+    [SerializeField] private string _name;
+    public Sprite Icon;
+    public string ItemName{
+        get{
+            return _name;
+        }
+        set {
+            _name = value;
+            this.name = value;
+        }
+    }
+    
+    public ItemType Type;
+    public ItemMaterial Material;
+    public float Quality;
+    
+    
+    public Item(ItemType type, ItemMaterial material, float quality, Sprite icon = null){
+        Type = type;
+        Material = material;
+        Quality = quality;
+        Icon = icon; 
+    }
+}
