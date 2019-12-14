c1f9c46a445d707122a6a699034cabf9a2e19927
Sam Gates <Sam.Gates@heliosinteractive.com>
2019-12-13 23:07:57 -0500

    Starting to add character movement and controls!

---
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BackwardsCap
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Rigidbody2D PlayerRB;
+        public PlayerAnimator PlayerAnimator;
+        private float speed = 10f;
+
+        [HideInInspector] public bool HasControl = true;
+
+        private Vector2 movement;
+
+
+        void Update()
+        {
+            if (!HasControl)
+            {
+                PlayerRB.velocity = Vector2.zero;
+                return;
+            }
+
+            Movement();
+        }
+
+
+
+        private void Movement()
+        {
+            movement = Vector2.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                movement.y += 1f;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                movement.y -= 1f;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                movement.x += 1f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                movement.x -= 1f;
+            }
+
+            PlayerRB.velocity = movement.normalized * speed;
+            PlayerAnimator.UpdatePlayerAnimator(movement);
+        }
+
+
+
+    } 
+}
