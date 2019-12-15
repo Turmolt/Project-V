2abdebdd19ad25f289487680f5b9b194413959bb
RJ Perry <raymond.perry@maine.edu>
2019-12-15 13:27:06 -0500

    NPC Spots Prefab created

---
+++ Assets/_Project/Scripts/NPCMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class NPCMovement : MonoBehaviour
+{
+    public GameObject[] Path1;
+    public GameObject[] Path2;
+    public GameObject[] Path3;
+    public GameObject[] Path4;
+    public GameObject[] Path5;
+    public GameObject[] Path6;
+    public GameObject[] Path7;
+    public GameObject[] Path8;
+
+
+
+    public void goHome(GameObject npc, GameObject[] path){
+        foreach (GameObject p in path){
+            //npc
+        }
+        
+
+    }
+    public void goTable(GameObject npc, GameObject[] path){
+
+    }
+
+}
