1d1f25cbec4182ce8923d952b441aef5a5d95c84
RJ Perry <raymond.perry@maine.edu>
2019-12-14 23:21:25 -0500

    Updated Quality to 0-5 scale Changed Quality Bar to segmented

--- Assets/_Project/Data/Item.cs
+++ Assets/_Project/Data/Item.cs
@@ -29,12 +29,13 @@ public class Item: MonoBehaviour   {
 
     public int Rarity;
 
-    [Range(0,100)][HideInInspector] public float Quality;
+    [HideInInspector] public float Quality;
 
     public void SetQuality(float quality)
     {
         Quality = quality;
         if (qualityBar != null) qualityBar.Quality = Quality;
+        
     }
 
     public float GetQuality()
