36849807ad5496a480588373636a4c49acb42004
Grant Beals <nothing1560@gmail.com>
2019-12-15 11:44:20 -0500

    Added Title splash screen, finished intro animations.

--- Assets/_Project/Scripts/IntroControl.cs
+++ Assets/_Project/Scripts/IntroControl.cs
@@ -19,13 +19,31 @@ namespace BackwardsCap
 
         public AudioClip IntroClip;
 
+        public GameObject Skeleton;
+
+        public Camera IntroCamera;
+
+        public Camera GameCamera;
+
+        public GameObject logo;
+
+
+
        
 
         private IEnumerator WaitForIntro()
         {
             PlayerRenderer.enabled = false;
             yield return new WaitForSeconds(IntroClip.length);
-            Travel();
+            Travel1();
+        }
+
+        private IEnumerator WaitForTitle()
+        {
+            logo.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            logo.SetActive(false);
+            Travel2();
         }
 
         private void Awake()
@@ -33,20 +51,30 @@ namespace BackwardsCap
             StartCoroutine(WaitForIntro());
         }
 
-        void Travel()
+        void Travel1()
         {
             Player.HasControl = false;
             FadeToBlack.DOFade(1f, 1f).OnComplete(() =>
             {
                 Player.transform.position = Destination.position;
-                SoundManager.instance.PlaySomething(MusicChoice);
-                FadeToBlack.DOFade(0f, 1f).OnComplete(() =>
-                {
-                    PlayerRenderer.enabled = true;
-                    Player.HasControl = true;
-
-                });
             });
+            
+            StartCoroutine(WaitForTitle());
+        }
+
+        void Travel2()
+        {
+
+            IntroCamera.enabled = false;
+            GameCamera.enabled = true;
+            SoundManager.instance.PlaySomething(MusicChoice);
+            FadeToBlack.DOFade(0f, 1f).OnComplete(() =>
+            {
+                PlayerRenderer.enabled = true;
+                Player.HasControl = true;
+
+            });
+
         }
     }
 }
