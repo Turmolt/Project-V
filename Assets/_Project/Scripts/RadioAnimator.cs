1f9f5ca014139c42ce82b19908c9c007bafa8388
Grant Beals <nothing1560@gmail.com>
2019-12-14 21:00:10 -0500

    Changed SpriteAnimator.cs to RadioAnimator.cs Fixed Arch over Player Bug Added radio audio to radio sprite

--- Assets/_Project/Scripts/RadioAnimator.cs
+++ Assets/_Project/Scripts/RadioAnimator.cs
@@ -2,23 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteAnimator : MonoBehaviour
+public class RadioAnimator : MonoBehaviour
 {
     [SerializeField] private Sprite[] frameArray;
     private int currentFrame;
     private float timer;
+    private float persistantTimer;
+    private AudioClip radioClip;
 
+
+    private void Awake()
+    {
+        radioClip = GetComponent<AudioSource>().clip;
+    }
     private void Update()
     {
-        Debug.Log(currentFrame);
-        Debug.Log(timer);
         timer += Time.deltaTime;
+        persistantTimer += Time.deltaTime;
         if (timer >= 1f)
         {
             timer -= 1f;
             currentFrame = (currentFrame + 1) % frameArray.Length;
             gameObject.GetComponent<SpriteRenderer>().sprite = frameArray[currentFrame];
-
+        }
+        if (radioClip.length < persistantTimer)
+        {
+            this.gameObject.SetActive(false);
         }
     }
 }
